23266178ec7288218bf2274a5535341c37ce305a
Eleanor Davies <ellie@mavieson.co.uk>
2019-11-05 13:26:00 +0000

    ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, request, render_template, send_file
-import requests, datetime, shutil, os, glob, uuid, json
+import requests, datetime, shutil, os, glob, uuid, json, onos_connect
 
 app = Flask(__name__)
 
@@ -15,7 +15,14 @@ def load_config():
         config = load_json("config.json")
     except:
         config = load_json("config-default.json")
-  
+
+
+
+@app.route('/api/get_intents', methods=['GET'])
+def get_intents():
+    
+    return make_response("UUID Not Found", 403)
+
 
 if __name__ == '__main__':
     load_config()
