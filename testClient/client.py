23fe27f24f8b81e1bc3a876b5548089931656f85
Eleanor Davies <ellie@mavieson.co.uk>
2020-04-02 14:34:28 +0100

    marco demo

--- testClient/client.py
+++ testClient/client.py
@@ -17,9 +17,9 @@ def post_json(url, reroute_JSON):
 
 
 def main():
-    with open("intent.json", 'r') as f:
+    with open("reroute.json", 'r') as f:
         reroute_JSON = json.load(f)
-        post_json('http://127.0.0.1:5000/api/push_intent',
+        post_json('http://api-demo.ng-cdi.com/api/push_intent',
                   json.dumps(reroute_JSON))
 
 
